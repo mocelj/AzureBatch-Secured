6ea3c71a2441b3644a96be92c1e35e6f793aa60e
mocelj <mocelj@darkos-macbook-air.home>
2021-12-15 20:49:45 +0100

    updated remote container build

--- artefacts/checkAkv/buildRemoteContainer.sh
+++ artefacts/checkAkv/buildRemoteContainer.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
+echo "Started building image $2 on registry $1"
+echo "Download artefacts..."
+wget https://raw.githubusercontent.com/mocelj/AzureBatch-Secured/main/artefacts/checkAkv/checkAkv.tgz   
+echo "Extracting artefacts..."
+tar xvzf checkAkv.tgz
 echo "Building image $2 on registry $1"
-
-wget https://raw.githubusercontent.com/mocelj/AzureBatch-Secured/main/artefacts/checkAkv/checkAkv.tgz      
-
-echo "Building image $2 on registry $1"
-
 fullImage="$2:latest"
-
-#az acr build --registry $1 --image $fullImage .  
-
+az acr build --registry $1 --image $fullImage .  
 echo 'Build finished'
